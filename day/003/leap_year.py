1c074141c7f6bcbd690d4716507bffeeb59fd5f1
Wanzaz <ondra.pazourek@gmail.com>
2022-03-13 18:54:49 +0100

    checks if it leap year or not with the leap rules

---
+++ day/003/leap_year.py
@@ -0,0 +1,28 @@
+# My solution
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 != 0:
+        print("Leap year.")
+    elif year % 400 == 0:
+        print("Leap year.")
+    else:
+        print("Not leap year.")
+else:
+    print("Not leap year.")
+
+"""
+# Angelas solution
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 != 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Not leap year.")
+else:
+    print("Not leap year.")
+"""
