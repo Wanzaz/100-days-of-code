906e3ad80a27196ccab0b428c71a2197e83ee4b5
Wanzaz <ondra.pazourek@gmail.com>
2022-03-13 18:58:19 +0100

    adds additional notes

---
+++ day/003/logical_operators.py
@@ -0,0 +1,15 @@
+# and, or, not
+
+# and
+a = 12
+if 15 > a and a > 10:
+    pass
+# both of them has to be True to be True otherwise False
+
+# or
+# one of them has to be True to be True otherwise False
+
+# not
+# reverse 
+not a > 15
+
