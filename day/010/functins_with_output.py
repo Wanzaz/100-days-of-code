756091b28ddfa76f07a029e95eb1532698889ddf
Wanzaz <ondra.pazourek@gmail.com>
2022-04-03 19:40:28 +0200

    adds the docsting to a function

--- day/010/functins_with_output.py
+++ day/010/functins_with_output.py
@@ -1,5 +1,6 @@
 
 def format_name(f_name, l_name):
+	"""Take a first and last name and format it to return the title case version of the name.""" # docstings
 	if f_name == "" or l_name == "":
 		return "You didn't provide valid inputs."
 	
