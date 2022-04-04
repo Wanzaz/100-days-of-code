53f43890fa7c688c4793e1048a0284ccf5072d8b
Wanzaz <ondra.pazourek@gmail.com>
2022-04-04 10:07:07 +0200

    adds comunication with user to while loop

--- day/009/auction_program.py
+++ day/009/auction_program.py
@@ -1,21 +1,17 @@
 import os
 
 bidder_dictonary = {}
+should_continue = True
 
-user_name = input("What is your name?: ")
-user_bid = int(input("What's you bid? $"))
-other_bidders = input("Are there any other bidders? Type 'yes' or 'no': ")
-os.system('clear')
-
-bidder_dictonary[user_name] = user_bid
-
-while other_bidders == 'yes':
+while should_continue:
 	# If you are running Windows use: os.system('cls') instead of os.system('clear')
 	os.system('clear')
 	user_name = input("What is your name?: ")
 	user_bid = int(input("What's you bid? $"))
 	other_bidders = input("Are there any other bidders? Type 'yes' or 'no': ")
 	bidder_dictonary[user_name] = user_bid
+	if other_bidders == 'no':
+		should_continue = False
 
 def find_highest_bid(bid_record):
 	biggest_bid = bidder_dictonary[user_name]
