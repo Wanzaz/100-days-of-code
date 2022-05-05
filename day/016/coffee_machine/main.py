f65d0ccf630919d4a7080dec5b1974ecb24acbfa
Wanzaz <ondra.pazourek@gmail.com>
2022-05-05 20:40:35 +0200

    adds coffee machine program; almost working but drink.cost doesn't work but it should work

---
+++ day/016/coffee_machine/main.py
@@ -0,0 +1,23 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    print(options)
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
