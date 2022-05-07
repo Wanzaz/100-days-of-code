a099488d47cd8fcf39fff47bf2e5a49f25af7717
Wanzaz <ondra.pazourek@gmail.com>
2022-05-07 22:50:14 +0200

    adds full working quiz game

--- day/017/quiz/main.py
+++ day/017/quiz/main.py
@@ -1,13 +1,18 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
+
 
 question_bank = []
 
-for q in question_data:
-    new_question = Question(q["text"], q["answer"])
+for question in question_data:
+    new_question = Question(question["text"], question["answer"])
     question_bank.append(new_question)
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
 
+while quiz.still_has_questions():
+    quiz.next_question()
 
+print(f"You have completed the quiz\nYour final score was: {quiz.score}/{quiz.question_number}.")
 
