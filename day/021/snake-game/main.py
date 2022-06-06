5ef73fcb42c05eeff136b520389c6bd1df591ca1
Wanzaz <ondra.pazourek@gmail.com>
2022-06-06 22:30:55 +0200

    adds snake game with food ; but not done

---
+++ day/021/snake-game/main.py
@@ -0,0 +1,45 @@
+import time
+from turtle import Screen
+from snake import Snake
+from food import Food
+
+
+"""
+Parts of this project
+1. Snake body
+2. Move the snake
+3. Create snake food
+4. Detect collision with food
+5. Create a scoreboard
+6. Detect collision with wall
+7. Detect collision with tail
+"""
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
+
+screen.exitonclick()
