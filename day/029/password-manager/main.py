c5907db55cc191feef3a90335f9106e7035acae3
Wanzaz <ondra.pazourek@gmail.com>
2022-08-13 14:16:22 +0200

    Add password-generator
    The password-manager program is complete now

--- day/029/password-manager/main.py
+++ day/029/password-manager/main.py
@@ -1,7 +1,24 @@
+import pyperclip
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = [choice(letters) for _ in range(randint(8, 10))]
+    password_list += [choice(symbols) for _ in range(randint(2, 4))]
+    password_list += [choice(numbers) for _ in range(randint(2, 4))]
+    # Or we could put them into separated lists and then do this: password_list = list1 + list2 + list3
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_input_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -22,7 +39,7 @@ def save():
 
             website_input_entry.delete(0, END)
             password_input_entry.delete(0, END)
-            website_input_entry.focus() # cursor in the first entry
+            website_input_entry.focus()  # cursor in the first entry
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -38,7 +55,7 @@ canvas.create_image(100, 100, image=logo_image)
 canvas.grid(column=1, row=0)
 
 # Buttons
-gen_pass_button = Button(text='Generate Password')
+gen_pass_button = Button(text='Generate Password', command=generate_password)
 gen_pass_button.grid(column=2, row=3)
 add_button = Button(text='Add', width=38, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
@@ -60,8 +77,6 @@ email_label = Label(text='Email/Username:')
 email_label.grid(column=0, row=2)
 password_label = Label(text='Password:')
 password_label.grid(column=0, row=3)
-
-
 
 
 window.mainloop()
