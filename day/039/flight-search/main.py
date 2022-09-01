0b450c5b18fbfef6104589443ad953e6338416da
Wanzaz <ondra.pazourek@gmail.com>
2022-09-01 16:11:28 +0200

    Add prices of the flights

--- day/039/flight-search/main.py
+++ day/039/flight-search/main.py
@@ -2,6 +2,7 @@ import os
 import requests
 from pprint import pprint
 from dotenv import load_dotenv, find_dotenv
+from datetime import datetime, timedelta
 load_dotenv(find_dotenv())
 
 
@@ -9,15 +10,31 @@ from data_manager import DataManager
 from flight_search import FlightSearch
 
 data_manager = DataManager()
+flight_search = FlightSearch()
 sheet_data = data_manager.get_destination_data()
+
+ORIGIN_CITY_IATA = "LON"
 
 
 if sheet_data[0]["iataCode"] == "":
-    flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
     pprint(sheet_data)
-
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
 
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+
+
+# if cheaper:
+    # send SMS
