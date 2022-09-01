30a4afcad8f5a75504c9fa26f671d66b45b8cd2a
Wanzaz <ondra.pazourek@gmail.com>
2022-09-01 14:06:48 +0200

    Add filling IATA Code column with original IATA Codes

--- day/039/flight-search/flight_search.py
+++ day/039/flight-search/flight_search.py
@@ -4,13 +4,23 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 TEQUILA_API_KEY = os.environ.get("TEQUILA_KIWI_API_KEY")
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 
 
 class FlightSearch:
 
     def get_destination_code(self, city_name):
-        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
-        code = "TESTING"
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+
+        search_response = requests.get(
+                url=location_endpoint,
+                headers=headers,
+                params=query
+        )
+        results = search_response.json()["locations"]
+        code = results[0]["code"]
         return code
 
 
