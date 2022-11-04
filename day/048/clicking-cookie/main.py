7be78db4041ec1b6155fb1c6ccb08ddccc476c88
Wanzaz <ondra.pazourek@gmail.com>
2022-11-04 16:43:35 +0100

    Add 1 minute clicking strategy for max cps

--- day/048/clicking-cookie/main.py
+++ day/048/clicking-cookie/main.py
@@ -2,39 +2,53 @@ import time
 import threading
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-
 
 chrome_driver_path = "/Users/ondrejpazourek/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
-
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
+
+def checking_for_upgrades():
+    """Looks for the most expesive feature available every five seconds"""
+
+    cookie_amount = int(driver.find_element(By.ID, "money").text.replace(",", ""))
+    # store = driver.find_element(By.ID, "store")
+    store_elements = driver.find_elements(By.TAG_NAME, "b")
+    prices = []
+    for element in store_elements[10:18]:
+        price_statement = element.text.split()
+        price = element.text.split()[len(price_statement) - 1]
+        price_trimmed = int(price.replace(",", ""))
+        prices.append(price_trimmed)
+
+    i = 0
+    max_price = 0
+    for price in prices:
+        i += 1
+        if price > max_price and price < cookie_amount:
+            max_price = i
+
+    store_elements = ["buyCursor", "buyGrandma", "buyFactory", "buyMine", "buyShipment", "buyAlchemy lab", "buyPortal", "buyTime machine"]
+
+    upgrade_to_buy = driver.find_element(By.ID, store_elements[max_price - 1])
+
+    upgrade_to_buy.click()
+
+
+    threading.Timer(5.0, checking_for_upgrades).start()
+
+
+timeout = time.time() + 1*60   # 1 minutes from now
+
+checking_for_upgrades()
+
 cookie = driver.find_element(By.ID, "cookie")
-cookie_amount = driver.find_element(By.ID, "money").text
-cps = driver.find_element(By.ID, "cps")
 
-# def checking_for_upgrades():
-#     """Looks for the most expesive feature available every five seconds"""
-#     cookie_amount = driver.find_element(By.ID, "money").text
-#     store = driver.find_element(By.ID, "store")
-#     store_elements = driver.find_elements(By.TAG_NAME, "b")
-#     for element in store_elements[11:18]:
-#         price_statement = element.text.split()
-#         price = element.text.split()[len(price_statement) - 1]
-#         price_trimmed = int(price.replace(",", ""))
-#         print(price_trimmed)
-#     # if 
-
-#     threading.Timer(5.0, checking_for_upgrades).start()
-
-
-timeout = time.time() + 5*60   # 5 minutes from now
-
-# checking_for_upgrades()
 while True:
     test = 0
     if test == 5 or time.time() > timeout:
+        cps = driver.find_element(By.ID, "cps").text
+        print(f"Cookies per second after 1 minute:\n {cps}")
         break
 
     test -= 1
@@ -43,6 +57,4 @@ while True:
 
 
 
-
-# driver.close()
 driver.quit()
