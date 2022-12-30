4e518004d58e22c4d8ca9e8dfcc0800985cbc8a5
Wanzaz <ondra.pazourek@gmail.com>
2022-12-30 13:34:44 +0100

    Add complete working solution to 052/

--- day/052/ig-follower-bot/main.py
+++ day/052/ig-follower-bot/main.py
@@ -1,3 +1,6 @@
+# Program is designed to find a certain account and follow all his following people
+# ERROR-INFO -- if it crashes the cause of it is Instagram follow per hour limitation
+
 import os
 from time import sleep
 from selenium import webdriver
@@ -5,9 +8,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
 
 
 from dotenv import load_dotenv, find_dotenv
@@ -46,12 +46,21 @@ class InstaFollower:
 
         login_button = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[3]')
         login_button.click()
-        sleep(5)
+        sleep(3)
+
+
+    def follow(self):
+        follow_buttons = self.driver.find_elements(By.XPATH, "//*[text()='Follow']")
+
+        for button in follow_buttons:
+            button.click()
+            sleep(1)
+        sleep(3)
 
 
     def find_followers(self):
         self.driver.get(IG_USC_LINK)
-        sleep(3)
+        sleep(2)
 
         following = self.driver.find_element(By.PARTIAL_LINK_TEXT, "following")
         following.click()
@@ -60,21 +69,16 @@ class InstaFollower:
         pop_up_window = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]')
   
         while True:
+            self.follow()
             self.driver.execute_script(
                 'arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', 
               pop_up_window)
             sleep(1)
-
-        sleep(10)
+        sleep(4)
 
         self.driver.quit()
-
-
-    def follow(self):
-        pass
 
 
 insta_bot = InstaFollower()
 insta_bot.login()
 insta_bot.find_followers()
-insta_bot.follow()
