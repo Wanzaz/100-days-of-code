e6678086f2cf2814e51c3ad60c43c091d8b7df4b
Wanzaz <ondra.pazourek@gmail.com>
2023-01-09 17:32:58 +0100

    Add my first app in Flask

---
+++ day/054/hello_flask/hello.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import webbrowser as wb
+
+
+app = Flask(__name__)
+
+# have to run: export FLASK_APP=hello.py
+
+# Python Decorator
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+wb.open_new_tab('http://127.0.0.1:5000/')
+
+if __name__ == "__main__":
+    app.run()
+
