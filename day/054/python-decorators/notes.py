e4c4a8ee5d1ca70f292768a2b3238a4c63274a6a
Wanzaz <ondra.pazourek@gmail.com>
2023-01-09 17:32:07 +0100

    Add python-decorators theory + examples

---
+++ day/054/python-decorators/notes.py
@@ -0,0 +1,45 @@
+## ********Day 54 Start**********
+## Functions can have inputs/functionality/output
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+##Functions are first-class objects, can be passed around as arguments e.g. int/string/float etc.
+
+def calculate(calc_function, n1, n2):
+    return calc_function(n1, n2)
+
+result = calculate(add, 2, 3)
+# print(result)
+
+##Functions can be nested in other functions
+
+def outer_function():
+    print("I'm outer")
+
+    def nested_function():
+        print("I'm inner")
+
+    nested_function()
+
+# outer_function()
+
+## Functions can be returned from other functions
+def outer_function():
+    print("I'm outer")
+
+    def nested_function():
+        print("I'm inner")
+
+    return nested_function
+
+inner_function = outer_function()
+inner_function()
