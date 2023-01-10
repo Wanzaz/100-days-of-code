0f00a8dcfaffa0db1bd85ac3e3f54120504e38f3
Wanzaz <ondra.pazourek@gmail.com>
2023-01-10 16:34:29 +0100

    Add simple decorator functions

--- day/054/python-decorators/notes.py
+++ day/054/python-decorators/notes.py
@@ -41,5 +41,33 @@ def outer_function():
 
     return nested_function
 
-inner_function = outer_function()
-inner_function()
+# inner_function = outer_function()
+# inner_function()
+
+
+## Simple Python Decorator Functions
+import time
+
+def delay_decorator(function):
+    def wrapper_function():
+        time.sleep(2)
+        #Do something before
+        function()
+        function()
+        #Do something after
+    return wrapper_function
+
+@delay_decorator
+def say_hello():
+    print("Hello")
+
+#With the @ syntactic sugar
+@delay_decorator
+def say_bye():
+    print("Bye")
+
+#Without the @ syntactic sugar
+def say_greeting():
+    print("How are you?")
+decorated_function = delay_decorator(say_greeting)
+decorated_function()
