2c84c31af389d3659e6be4bab875c684406b1c83
Wanzaz <ondra.pazourek@gmail.com>
2023-01-10 16:36:16 +0100

    Add say_bye() to /bye domain

--- day/054/hello_flask/hello.py
+++ day/054/hello_flask/hello.py
@@ -11,8 +11,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/bye')
+def say_bye():
+    return 'Bye'
+
 wb.open_new_tab('http://127.0.0.1:5000/')
 
 if __name__ == "__main__":
     app.run()
-
