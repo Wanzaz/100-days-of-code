583be1463b79e5324e741c78f6f3d947cb3b4b87
Wanzaz <ondra.pazourek@gmail.com>
2023-01-15 17:16:47 +0100

    Rendering html elements with Flask

--- day/055/higher_lower_game/main.py
+++ day/055/higher_lower_game/main.py
@@ -10,7 +10,9 @@ app = Flask(__name__)
 # Python Decorator
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1 style="text-align: center">Hello, World!</h1>' \
+           '<p>This is paragraph.</p>' \
+           '<img src="https://media4.giphy.com/media/vMbC8xqhIf9ny/giphy.gif?cid=ecf05e47cf62nifs7w2i9p3vi04b3doxzxzvs6gjtnqe6c8d&rid=giphy.gif&ct=g" width=200>'
 
 
 # Different routes using the app.route decorator
